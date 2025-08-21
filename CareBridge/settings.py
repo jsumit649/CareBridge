c1d660d76f7b59af72c7f7bff0ed9182da65528d
Sumit Jha <jsumit649@gmail.com>
2025-08-21 22:31:15 +0530

    Refactor settings for environment variable management, enhance footer layout, and improve responsive design in CSS

--- CareBridge/settings.py
+++ CareBridge/settings.py
@@ -11,21 +11,19 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(qg7)m5lma88oj3==0*mtzlafcb*=or5dssfvpmm1f^@p3!9+%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
 AUTH_USER_MODEL = 'main.User'
 
@@ -86,11 +84,11 @@ WSGI_APPLICATION = 'CareBridge.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'carebridge',
-        'USER': 'postgres',
-        'PASSWORD': 'Sumit@123',
-        'HOST': 'localhost',
-        'PORT': 5432
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT', cast=int),
     }
 }
     
