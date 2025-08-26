a3542bc861dc2994212e037bfd323713a3234daa
Sumit Jha <jsumit649@gmail.com>
2025-08-26 16:00:17 +0530

    Register Doctor and AuditLog models in admin; remove CSRF tokens from forms in multiple templates

--- main/admin.py
+++ main/admin.py
@@ -6,3 +6,6 @@ from . import models
 admin.site.register(models.User)
 admin.site.register(models.Patient)
 admin.site.register(models.Doctor)
+admin.site.register(models.PatientDoctorMapping)
+admin.site.register(models.AuditLog)
+
